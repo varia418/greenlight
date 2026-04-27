5bdf2a7c329ede843a666f90962328bf526c20ee
varia <ltnhan2610@gmail.com>
2026-04-27 15:11:11 +0700

    Use httproute package and create writeJSON helper

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -35,12 +35,9 @@ func main() {
 		logger: logger,
 	}
 
-	mux := http.NewServeMux()
-	mux.HandleFunc("/v1/healthcheck", app.healthcheckHandler)
-
 	srv := &http.Server{
-		Addr:         fmt.Sprintf(":%d", cfg.port),
-		Handler:      mux,
+		Addr:         fmt.Sprintf("127.0.0.1:%d", cfg.port),
+		Handler:      app.routes(),
 		IdleTimeout:  time.Minute,
 		ReadTimeout:  10 * time.Second,
 		WriteTimeout: 30 * time.Second,
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module greenlight.varia
 
 go 1.26.2
+
+require github.com/julienschmidt/httprouter v1.3.0
