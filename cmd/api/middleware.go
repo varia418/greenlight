cea46bf545d52341ec0159e0fd0f4b8c10c714c6
varia <ltnhan2610@gmail.com>
2026-05-13 23:09:13 +0700

    Detect real ip

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -3,12 +3,12 @@ package main
 import (
 	"errors"
 	"fmt"
-	"net"
 	"net/http"
 	"strings"
 	"sync"
 	"time"
 
+	"github.com/tomasen/realip"
 	"golang.org/x/time/rate"
 	"greenlight.varia/internal/data"
 	"greenlight.varia/internal/validator"
@@ -149,11 +149,7 @@ func (app *application) rateLimit(next http.Handler) http.Handler {
 
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if app.config.limiter.enabled {
-			ip, _, err := net.SplitHostPort(r.RemoteAddr)
-			if err != nil {
-				app.serverErrorResponse(w, r, err)
-				return
-			}
+			ip := realip.FromRequest(r)
 
 			mu.Lock()
 
